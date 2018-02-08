fb17dea7de8c82b0148f01eb4cfaadae9f95db28
hevlitt <hoedvaldezlo@ittepic.edu.mx>
2018-02-08 01:09:26 -0700

    Initial commit

---
+++ app/src/main/java/com/example/desk/juegogato/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.desk.juegogato;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,btn10;
+    int x=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn=(Button)findViewById(R.id.button);
+        btn2=(Button)findViewById(R.id.button2);
+        btn3=(Button)findViewById(R.id.button3);
+        btn4=(Button)findViewById(R.id.button4);
+        btn5=(Button)findViewById(R.id.button5);
+        btn6=(Button)findViewById(R.id.button6);
+        btn7=(Button)findViewById(R.id.button7);
+        btn8=(Button)findViewById(R.id.button8);
+        btn9=(Button)findViewById(R.id.button9);
+        btn10=(Button)findViewById(R.id.button10);
+
+
+    }
+
+    public void laun(){
+        if(x==0){
+            btn.setText("Marco");
+        }
+    }
+}
